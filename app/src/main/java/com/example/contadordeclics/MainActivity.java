d20628d60eb7b2624511f978ee9f1f89a207ae8c
Gedi <gordoneza01@elpuig.xeill.net>
2021-10-27 20:21:43 +0200

    .

---
+++ app/src/main/java/com/example/contadordeclics/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.contadordeclics;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    int contador1;
+    int contador2;
+    int contador3;
+    int contador4;
+    int contadorglobal;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
+
+
+
+
+
+
